3ad9f7f6c88df4e9636ef0672cf2fce766947854
Nsamba Taufeeq <nsambataufeeq@gmail.com>
2025-05-09 03:33:58 +0300

    Set the base path for Slim application in index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
+// Set the base path for Slim
+$app->setBasePath('/local/certhub/index.php');
+
 // Add middleware
 (require __DIR__ . '/middleware/AuthMiddleware.php')($app);
 
